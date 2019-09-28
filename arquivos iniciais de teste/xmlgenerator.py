710a9136a25ed4067f0753e51e7a4167daab97fc
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2019-09-27 23:21:17 -0300

    separando função tex2html em um módulo

--- arquivos iniciais de teste/xmlgenerator.py
+++ arquivos iniciais de teste/xmlgenerator.py
@@ -1,6 +1,6 @@
 import xml.etree.ElementTree as ET
 import os
-import re
+from t2h import tex2html
 
 
 def spacei():
@@ -9,59 +9,6 @@ def spacei():
 
 def spacef():
     return '<br /><br />\n\n'
-
-
-# Lida parcialmente com a tradução do texto em tex para html
-def tex2html(s):
-    """
-    Return a string with syntax substitutions,
-    from latex format to html, performed on the
-    given input
-
-    rules -- dict with all the substiturion rules
-             for a specific format
-    s -- input string to be formated
-    """
-    rules1 = {
-        r'\\begin{itemize}': '<ul>',
-        r'\\end{itemize}': '</ul>',
-        r'\\lt': '&lt;',
-        r'\\le': '&le;',
-        r'\\gt': '&gt;',
-        r'\\ge': '&ge;',
-        r'\\arrowvert': '|',
-        r'\\\^': '^'
-    }
-
-    rules2 = {
-        r'\\emph': 'i',
-        r'\\textbf': 'b',
-        r'\\textit': 'i'
-        # r'\\textsc': '',
-        # r'\\texttt': '',
-        # r'\\textsf': '',
-        # r'\\textrm': '',
-        # r'\\textsl': ''
-    }
-
-    rules3 = {
-        r'\\item': 'li'
-    }
-
-    for l, h in rules1.items():
-        s = re.sub(l, h, s)
-
-    for l, h in rules2.items():
-        l_str = r'%s{([^}]*)}' % l
-        h_str = r'<%s>\1</%s>' % (h, h)
-        s = re.sub(l_str, h_str, s)
-
-    for l, h in rules3.items():
-        l_str = r'%s([^\n]*)' % l
-        h_str = r'<%s>\1</%s>' % (h, h)
-        s = re.sub(l_str, h_str, s)
-
-    return s
 
 
 # Templates para a questão e para casos teste
