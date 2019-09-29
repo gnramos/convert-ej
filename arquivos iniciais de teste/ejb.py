3ee3dbcadcfc13c1492bdb9c94cd6f6928ec4563
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2019-09-29 14:46:58 -0300

    nome do programa

---
+++ arquivos iniciais de teste/ejb.py
@@ -0,0 +1,27 @@
+import argparse
+from xmlgenerator import xml_gen
+from xmlsgenerator import xmls_gen
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="EJBridge")
+
+    parser.add_argument("-d", "--directory", type=str, nargs=1,
+                        metavar="name_directory", default=None,
+                        help="Directory name with all the questions")
+
+    parser.add_argument("-q", "--question", type=str, nargs=1,
+                        metavar="name_question", default=None,
+                        help="Name of the the question")
+
+    args = parser.parse_args()
+
+    if args.question:
+        xml_gen(args.question[0])
+    elif args.directory:
+        xmls_gen(args.directory[0])
+
+
+if __name__ == "__main__":
+    main()
