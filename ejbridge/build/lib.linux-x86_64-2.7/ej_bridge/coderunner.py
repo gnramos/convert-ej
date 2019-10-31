1706e0abbc08903e88701ff334dbdc45a84347f3
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2019-10-31 20:33:49 -0300

    gerar questões a partir do arquivo zipado .zip closes #13

--- ejbridge/build/lib.linux-x86_64-2.7/ej_bridge/coderunner.py
+++ ejbridge/build/lib.linux-x86_64-2.7/ej_bridge/coderunner.py
@@ -1,5 +1,7 @@
 import xml.etree.ElementTree as ET
 import os
+import zipfile
+import shutil
 from .t2h import tex2html
 
 
@@ -150,7 +152,11 @@ def xml_gen(directory, question_name):
 
 
 def dir_cf_to_cr(directory):
+
     questions = os.listdir(directory)
     for name in questions:
-        if os.path.isdir(directory + '/' + name):
-            xml_gen(directory + '/' + name, name)
+        if name.endswith('.zip'):
+            with zipfile.ZipFile(directory + '/' + name, 'r') as zip_ref:
+                zip_ref.extractall('tmp')
+            xml_gen('tmp', name[:-4])
+            shutil.rmtree('tmp')
