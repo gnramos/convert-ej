f6ae54f7ef6f8412361b73f7755043c12382a78d
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2019-11-10 20:21:41 -0300

    uso do os.file.path() |closes #20|

--- ejbridge/build/lib.linux-x86_64-2.7/ej_bridge/coderunner.py
+++ ejbridge/build/lib.linux-x86_64-2.7/ej_bridge/coderunner.py
@@ -38,17 +38,17 @@ def get_section(file_name, sec_name):
 
 def intermediate_to_coderunner(directory, question_name):
     # Templates para a questão e para casos teste
-    package_dir = os.path.abspath(os.path.dirname(__file__)) + '/'
-    tree = ET.parse(package_dir + 'Template.xml')
+    package_dir = os.path.abspath(os.path.dirname(__file__))
+    tree = ET.parse(os.path.join(package_dir, 'Template.xml'))
     root = tree.getroot()
     root = root[0]
-    testtree = ET.parse(package_dir + 'Testcase-Template.xml')
+    testtree = ET.parse(os.path.join(package_dir, 'Testcase-Template.xml'))
     testoroot = testtree.getroot()
 
     texto = ''  # Texto da questão
-    dir_sec = directory + '/text/'  # Diretório do texto
+    dir_sec = os.path.join(directory, 'text')  # Diretório do texto
     # Insere o nome
-    with open(dir_sec+'name.tex', 'r') as name:
+    with open(os.path.join(dir_sec, 'name.tex'), 'r') as name:
         root.find("name").find("text").text = name.read()
 
     sections = {
@@ -59,8 +59,9 @@ def intermediate_to_coderunner(directory, question_name):
     }
     # Formata os textos
     for file_name, sec_name in sections.items():
-        if os.path.isfile(dir_sec + file_name + '.tex'):
-            texto += get_section(dir_sec + file_name + '.tex', sec_name)
+        if os.path.isfile(os.path.join(dir_sec, file_name + '.tex')):
+            texto += get_section(os.path.join(dir_sec, file_name + '.tex'),
+                                 sec_name)
 
     files_sec = os.listdir(dir_sec)
     for name in files_sec:
@@ -70,7 +71,7 @@ def intermediate_to_coderunner(directory, question_name):
             img.set('path', '/')
             img.set('encoding', 'base64')
 
-            with open(dir_sec + '/' + name, "rb") as image:
+            with open(os.path.join(dir_sec, name), "rb") as image:
                 encoded_string = str(b64encode(image.read()), 'utf-8')
 
             img.text = encoded_string
@@ -80,24 +81,25 @@ def intermediate_to_coderunner(directory, question_name):
     root.find("questiontext").find("text").append(CDATA(texto))
 
     # Insere tutorial
-    if os.path.isfile(dir_sec + 'tutorial.tex'):
-        with open(dir_sec + 'tutorial.tex', 'r') as t:
+    if os.path.isfile(os.path.join(dir_sec, 'tutorial.tex')):
+        with open(os.path.join(dir_sec, 'tutorial.tex'), 'r') as t:
             root.find("generalfeedback").find("text").text = tex2html(t.read())
 
-    with open(directory+'/type', 'r') as sol_file:
+    with open(os.path.join(directory, 'type'), 'r') as sol_file:
         solutiontype = sol_file.read()
     # Insere a solução na questão
-    name_dir = os.listdir(directory+'/solutions/')
+    name_dir = os.listdir(os.path.join(directory, 'solutions'))
     namesolution = name_dir[0]
 
     # Insere a linguagem de programação utilizada
     root.find("coderunnertype").text = solutiontype
 
-    with open(directory+'/solutions/' + namesolution, 'r') as solution:
+    with open(os.path.join(directory, 'solutions', namesolution), 'r') \
+            as solution:
         root.find("answer").append(CDATA(solution.read()))
 
     # Faz uma busca por todos os arquivos de teste e os ordena
-    tests = os.listdir(directory+'/testcases/')
+    tests = os.listdir(os.path.join(directory, 'testcases'))
     tests.sort()
 
     # Encontra os casos testes a serem visualizados pelo aluno
@@ -111,7 +113,8 @@ def intermediate_to_coderunner(directory, question_name):
     # e adiciona-o na questão
     for arq in tests:
         if arq.endswith('.a'):
-            with open(directory+'/testcases/'+arq, 'r') as testout:
+            with open(os.path.join(directory, 'testcases', arq), 'r') \
+                    as testout:
                 testoroot.find("expected").find("text").text = testout.read()
 
                 if arq in list_tests_show:
@@ -119,14 +122,16 @@ def intermediate_to_coderunner(directory, question_name):
 
                 root.find("testcases").append(testoroot)
 
-                testtree = ET.parse(package_dir + 'Testcase-Template.xml')
+                testtree = ET.parse(os.path.join(package_dir,
+                                                 'Testcase-Template.xml'))
                 testoroot = testtree.getroot()
         else:
-            with open(directory+'/testcases/'+arq, 'r') as testinput:
+            with open(os.path.join(directory, 'testcases', arq), 'r') \
+                    as testinput:
                 testoroot.find("stdin").find("text").text = testinput.read()
 
     # Insere as tags
-    with open(directory+"/tags", 'r') as tagfile:
+    with open(os.path.join(directory, "tags"), 'r') as tagfile:
         tagscontent = tagfile.readlines()
     taglist = [x.strip() for x in tagscontent]
 
@@ -137,13 +142,13 @@ def intermediate_to_coderunner(directory, question_name):
         tags.append(tag)
 
     # Insere tempo e mémoria limite
-    with open(directory+'/time', 'r') as time_file:
+    with open(os.path.join(directory, 'time'), 'r') as time_file:
         root.find("cputimelimitsecs").text = time_file.read()
-    with open(directory+'/memory', 'r') as memory_file:
+    with open(os.path.join(directory, 'memory'), 'r') as memory_file:
         root.find("memlimitmb").text = memory_file.read()
 
     # Gera o arquivo final da questão
     files = 'Files'
     if not os.path.exists(files):
         os.mkdir(files)
-    tree.write(files + '/' + question_name + '.xml', 'UTF-8')
+    tree.write(os.path.join(files, question_name + '.xml'), 'UTF-8')
--- ejbridge/build/lib.linux-x86_64-2.7/ej_bridge/translation_functions.py
+++ ejbridge/build/lib.linux-x86_64-2.7/ej_bridge/translation_functions.py
@@ -5,13 +5,13 @@ from .codeforces import codeforces_to_intermediate
 from .coderunner import intermediate_to_coderunner
 
 
-def dir_cf_to_cr(directory):
-    questions = os.listdir(directory)
+def dir_cf_to_cr(dir):
+    questions = os.listdir(dir)
     for name in questions:
         if name.endswith('.zip'):
-            with zipfile.ZipFile(directory + '/' + name, 'r') as zip_ref:
+            with zipfile.ZipFile(os.path.join(dir, name), 'r') as zip_ref:
                 zip_ref.extractall('tmp')
             codeforces_to_intermediate('tmp', name[:-4])
-            intermediate_to_coderunner('intermadiate_files/' + name[:-4],
-                                       name[:-4])
+            intermediate_to_coderunner(os.path.join('intermadiate_files/',
+                                                    name[:-4]), name[:-4])
             shutil.rmtree('tmp')
