eaaeac80615425afe9ab0a87d05965d937f24df1
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2019-11-17 09:44:17 -0300

    transformação de imagens em .eps para .png |closes #8|

--- base64TEST/image.py
+++
@@ -1,9 +0,0 @@
-from base64 import b64encode
-
-path = "estacao.eps"
-
-with open(path, "rb") as image:
-    encoded_string = b64encode(image.read())
-
-with open("encoded.txt", 'wb') as result:
-    result.write(encoded_string)
