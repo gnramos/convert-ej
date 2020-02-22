f9dc99d56be8bd0c7666acbdf6832d1cf6077926
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2020-02-22 19:30:30 -0300

    criando funções de log e mudando o exemplo de pasta

--- ej_bridge/ejb.py
+++ ej_bridge/ejb.py
@@ -15,6 +15,26 @@ logging.basicConfig(
     datefmt='%m/%d/%Y - %I:%M:%S %p')
 
 
+def log_exceptions(err_list):
+    for error in err_list.args:
+        logging.error('Error: {}'.format(error))
+
+
+def log_fail(file):
+    logging.error('It was not possible to generate '
+                  'the question \'{}\'.'
+                  .format(os.path.splitext(file)[0]))
+
+
+def log_sucess(file):
+    logging.info('Question \'{}\' was successfully generated!'
+                 .format(os.path.splitext(file)[0]))
+
+
+def log_no_files():
+    logging.error('Path or file doesn\'t exist.')
+
+
 def _file_or_path_(path):
     """Return all the files inside path, in case it's a directory,
     otherwise if it's just a single file, return the file."""
@@ -35,6 +55,7 @@ def _parser_():
 
     subparsers = parser.add_subparsers(dest='command')
 
+    # Codeforces to Coderunner
     cf2cr = subparsers.add_parser('cf2cr',
                                   description='Translate CodeForces packages \
                                                to CodeRunner XML.',
@@ -56,6 +77,15 @@ def _parser_():
     cf2cr.add_argument('-l', '--language', choices=['c', 'cpp', 'python'],
                        default='c', help='Set programming language.')
 
+    # Codeforces to BOCA
+    cf2boca = subparsers.add_parser('cf2boca',
+                                    description='Translate CodeForces packages \
+                                                 to BOCA ZIP.',
+                                    help='Help on transforming CodeForces \
+                                          package(s) into BOCA ZIP(s).')
+    cf2boca.add_argument('files', type=_file_or_path_,
+                         help='Path of a file or a folder of files.')
+
     return parser
 
 
@@ -74,15 +104,13 @@ def main():
                     del cf
                     del cr
                 except Exception as err_list:
-                    for error in err_list.args:
-                        logging.error('Error: {}'.format(error))
-                    logging.error('It was not possible to generate '
-                                  'the question \'{}\'.'.format(file[:-4]))
+                    log_exceptions(err_list)
+                    log_fail(file)
                 else:
-                    logging.info('Question \'{}\' was successfully generated!'
-                                 .format(file[:-4]))
+                    log_sucess(file)
         if not args.files:
-            logging.error('Path or file doesn\'t exist.')
+            log_no_files()
+
     elif args.command == 'cf2boca':
         pass
 
