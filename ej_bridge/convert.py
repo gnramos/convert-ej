a4985f3066c3337fda3eb6b506ed812501d224e4
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2020-05-30 23:23:10 -0300

    refactoring parsing function - closes #42

--- ej_bridge/convert.py
+++ ej_bridge/convert.py
@@ -116,90 +116,94 @@ class PolygonReader(Parsing, readers.Polygon):
 
 
 ###############################################################################
-def base_parser():
-    """Creates a parser with default arguments and parses it."""
-    def list_input(path):
-        """Returns a list of the input files.
+class BaseParsing(Parsing):
+    def add_arguments(self, parser):
+        """Adds defaut command line arguments."""
 
-        Returns all the files inside path, in case it's a directory, otherwise
-        returns the single file path.
-        """
-        files = set()
-        if os.path.isfile(path):
-            files.add(path)
-        elif os.path.isdir(path):
-            with os.scandir(path) as it:
-                files = set(entry.path
-                            for entry in it
-                            if (entry.is_file() and
-                                not entry.name.startswith('.')))
-        else:
-            raise ArgumentTypeError('Must provide a valid file or directory')
-        return files
+        def list_input(path):
+            """Returns a list of the input files.
 
-    def check_dir(path):
-        """Check if the given path is of a directory."""
-        if os.path.isdir(path):
-            return path
-        raise ArgumentTypeError(f'{path} is not a directory')
+            Returns all the files inside path, in case it's a directory,
+            otherwise returns the single file path.
+            """
+            files = set()
+            if os.path.isfile(path):
+                files.add(path)
+            elif os.path.isdir(path):
+                with os.scandir(path) as it:
+                    files = set(entry.path for entry in it
+                                if (entry.is_file() and
+                                    not entry.name.startswith('.')))
+            else:
+                raise ArgumentTypeError('Must provide a valid file/directory')
+            return files
 
-    def list_formats(module):
-        return sorted([m[0]
-                       for m in inspect.getmembers(getattr(cur_module, module),
-                                                   inspect.isclass)
-                       if not inspect.isabstract(m[1])])
+        def check_dir(path):
+            """Check if the given path is of a directory."""
+            if os.path.isdir(path):
+                return path
+            raise ArgumentTypeError(f'{path} is not a directory')
 
-    cur_module = sys.modules[__name__]
+        def list_formats(module):
+            curr = sys.modules[__name__]
+            return sorted([m[0]
+                           for m in inspect.getmembers(getattr(curr, module),
+                                                       inspect.isclass)
+                           if not inspect.isabstract(m[1])])
 
-    # If required, the standard "help" action is processed in the 1st parsing
-    # step and then the program exits (as per argparse design), thus it would
-    # NOT show the e-judge-specific information that is added in runtime.
-    # Therefore, help is added only for the second parsing.
+        parser.add_argument('reader', choices=list_formats('readers'),
+                            help='Input e-judge format')
+        parser.add_argument('writer', choices=list_formats('writers'),
+                            help='Output e-judge format')
+        parser.add_argument('files', type=list_input,
+                            help='Path of a file or folder of files to convert'
+                            ' from reader to writer formats')
+        parser.add_argument('-o', '--output_dir', type=check_dir, default='./',
+                            help='Path of folder to save converted file(s)')
+
+
+###############################################################################
+def parse():
+    """Create a parser with default arguments, dynamically add sub-arguments
+    from the choosen formats and return reader/writer methods.
+
+    If required, the standard "help" action is processed in the first parsing
+    step and then the program exits (as per argparse design),
+    thus it would NOT show the e-judge-specific information that is added
+    in runtime. Therefore, help is added only for the second parsing step.
+    """
+
     parser = DefaultHelpParser(description='Convert between e-judge formats',
                                add_help=False,
                                formatter_class=RawTextHelpFormatter)
 
-    parser.add_argument('reader', choices=list_formats('readers'),
-                        help='Input e-judge format')
-    parser.add_argument('writer', choices=list_formats('writers'),
-                        help='Output e-judge format')
-    parser.add_argument('files', type=list_input,
-                        help='Path of a file or a folder of files to convert'
-                        ' from reader to writer formats')
-    parser.add_argument('-o', '--output_dir', type=check_dir, default='./',
-                        help='Path of folder to save converted file(s) into')
+    BaseParsing(parser)
 
-    return parser
-
-
-def get_instances(parser, args):
-    """Adds parser arguments and returns reader/writer methods."""
-    current_module = sys.modules[__name__]
-    reader = getattr(current_module, f'{args.reader}Reader')
-    writer = getattr(current_module, f'{args.writer}Writer')
-
-    return reader(parser), writer(parser)
-
-
-def main():
-    # Parsing arguments are added dynamically according to command line
-    # arguments, so there are 2 parsing steps.
-
-    # Add command line arguments for specifying reader/writer and source
-    # file(s).
-    parser = base_parser()
     args, unknown = parser.parse_known_args()
 
     if args.reader == args.writer:
         exit(0)
 
     # Get instances for specific reading/writing, which update the parser.
-    reader, writer = get_instances(parser, args)
+    curr_module = sys.modules[__name__]
+    reader_class = getattr(curr_module, f'{args.reader}Reader')
+    writer_class = getattr(curr_module, f'{args.writer}Writer')
+    reader = reader_class(parser)
+    writer = writer_class(parser)
 
     parser.add_argument('-h', '--help', action='help',
                         help='show this help message and exit')
 
     args = parser.parse_args()
+
+    return args, reader, writer
+
+
+def main():
+
+    # Parsing arguments are added dynamically according to command line
+    # arguments.
+    args, reader, writer = parse()
 
     # Process file(s).
     for file in args.files:
