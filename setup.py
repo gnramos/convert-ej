a5cd95b27c9ffe2a57c6e133c5161ac0ee90e176
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2020-07-20 18:24:28 -0300

    testing tags

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name="convert-ej",
-    version="1.1.1",
+    version="1.1.2",
     description="A software tool for converting problem files between electronic judges formats.",
     long_description=readme(),
     long_description_content_type="text/markdown",
